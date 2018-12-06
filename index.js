f23f335ced47e806c8d3e4298d1ad168f069d91d
James Robert Perih <james.perih@brewninja.net>
2018-12-06 17:01:12 -0600

    Support for fieldhouse.

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const request = require('request');
 const app = require('express')();
 var path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/html/index.html'));
-});
-
-app.get('/api/v1/lawson/aquatic/:dayOfWeek', function (req, res) {
-    // https://leisurefeedimages.blob.core.windows.net/lawson/6%20LAQC%20Saturday.png
-    const dayOfWeek = req.params.dayOfWeek;
+function getDayNumberForDayOfWeek(dayOfWeek) {
     let dayNumber = -1;
 
     switch (dayOfWeek) {
@@ -36,9 +30,29 @@ app.get('/api/v1/lawson/aquatic/:dayOfWeek', function (req, res) {
             dayNumber = 7;
             break;
     }
+
+    return dayNumber;
+}
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html/index.html'));
+});
+
+app.get('/api/v1/lawson/aquatic/:dayOfWeek', function (req, res) {
+    // https://leisurefeedimages.blob.core.windows.net/lawson/6%20LAQC%20Saturday.png
+    const dayOfWeek = req.params.dayOfWeek;
+    const dayNumber = getDayNumberForDayOfWeek(dayOfWeek);
     const URL = `https://leisurefeedimages.blob.core.windows.net/lawson/${dayNumber}%20LAQC%20${dayOfWeek}.png`;
     request.get(URL).pipe(res);
 });
+
+app.get('/api/v1/lawson/fieldhouse/:dayOfWeek', function (req, res) {
+    const dayOfWeek = req.params.dayOfWeek;
+    const dayNumber = getDayNumberForDayOfWeek(dayOfWeek);
+    const URL = `https://leisurefeedimages.blob.core.windows.net/fieldhouse/${dayNumber}%20FLDH%20${dayOfWeek}.png`;
+    request.get(URL).pipe(res);
+});
+
 
 app.listen(PORT, function() {
     console.log(`running on ${PORT}`);
